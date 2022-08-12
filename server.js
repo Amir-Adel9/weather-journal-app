2fea3a9cca74d3d4225fd23ad766ca5382424332
Amir-Adel9 <amiradel7152@gmail.com>
2022-08-12 23:09:30 +0200

    Pre-Final

---
+++ server.js
@@ -0,0 +1,40 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = [];
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+// Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+const { request, response } = require('express');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+// Setup the port
+const port = 8000;
+// Setup Server
+const server = app.listen(port, (request, response)=> {
+    console.log(`running on localhost: ${port}`);
+});
+// Setup get route  
+app.get('/all', (request, response)=>{
+    response.send(projectData);
+})
+// Setup post route
+app.post('/all', (request, response)=> {
+    newEntry = {
+        date: request.body.date,
+        temp: request.body.temp,
+        feeling: request.body.feeling
+    }
+    projectData.push(newEntry)
+    console.log(projectData)
+    response.send(projectData)
+})
---
+++ website/app.js
@@ -0,0 +1,69 @@
+/* Global Variables */
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
+const API_KEY = '&appid=339706ad2f2c34c3e0ac172484e163b8&units=imperial';
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+// Intializing Selectors
+const zipSelector = document.getElementById('zip');
+const feelingSelector = document.getElementById('feelings');
+const buttonSelector = document.getElementById('generate');
+const dateHolder = document.getElementById('date');
+const tempHolder = document.getElementById('temp');
+const feelingsHolder = document.getElementById('content');
+// Event listener for button
+buttonSelector.addEventListener('click', () => {
+    const zipcode = 'zip=' + zipSelector.value;
+    getWeather(baseURL, zipcode, API_KEY)
+    .then((response)=>{
+        postWeather('/all', {date: newDate, temp: response.main.temp, feeling: feelingSelector.value})
+    })
+    .then(
+        updateUI()
+    )
+});
+// Intializing getWeather function
+const getWeather = async (baseURL, zipcode, key,) => {
+    const response = await fetch(baseURL+ zipcode + key)
+    try {
+        const data = await response.json();
+        console.log(data)
+        return data;
+      }  catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+      }
+};
+// Intializing postWeather function
+const postWeather = async (url, data= {})=> {
+    console.log(data)
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+      });
+    try {
+        const data = await response.json();
+        console.log(data)
+        return data;
+      }  catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+      }
+};
+// Intializing updateUI function
+const updateUI = async ()=> {
+    const request = await fetch('/all')
+    try {
+        const allData = await request.json()
+        console.log(allData)
+        tempHolder.innerHTML = Math.round(allData[0].temp) + ' degrees'
+        feelingsHolder.innerHTML = allData[0].feeling
+        dateHolder.innerHTML = allData[0].date
+    } catch(error) {
+        console.log('error', error)
+    }
+}
